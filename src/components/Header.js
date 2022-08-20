c2e012abbe731c75f93258c18a863aa6741b64bc
jjgouveia <gouvik.dev@gmail.com>
2022-08-20 01:59:33 -0300

    requisito 8 finalizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title }) {
   return (
     <header>
-      <img
-        data-testid="profile-top-btn"
-        src={ profileIcon }
-        alt="Ícone do perfil"
-      />
+      <Link to="/profile">
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Ícone do perfil"
+        />
+      </Link>
       {(title !== 'Profile'
       && title !== 'Done Recipes'
       && title !== 'Favorite Recipes')
@@ -21,7 +24,6 @@ function Header({ title }) {
       />}
       <h1 data-testid="page-title">{title}</h1>
     </header>
-
   );
 }
 
---
+++ src/tests/Header.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+describe('Componente Header', () => { 
+    it('should ', () => {
+    });
+ })
