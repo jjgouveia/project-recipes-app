309c164a49f6c3e109f8a8a625e95f6f2fcedb14
Leandro Camargo <leandro.bcamargo@gmail.com>
2022-08-24 15:22:46 -0300

    requisito 19 incompleto

---
+++ src/components/Recipes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from './Card';
+import Header from './Header';
+
+const maxItens = 12;
+const Recipes = ({ apiResponse, pathname }) => (
+  <div>
+    {pathname === '/foods' && <Header title="Foods" />}
+    { pathname === '/foods' ? apiResponse.meals.slice(0, maxItens).map((meal, index) => (
+      <Card type={ meal } key={ index } index={ index } pathname="/foods" />
+    )) : apiResponse.drinks.slice(0, maxItens).map((drink, index) => (
+      <Card type={ drink } key={ index } index={ index } pathname="/drinks" />
+    ))}
+  </div>
+);
+
+Recipes.propTypes = {
+  apiResponse: PropTypes.shape(),
+  pathname: PropTypes.string,
+}.isRequired;
+
+export default Recipes;
