37fed5cc9819320cbef0f3748ae52dcc44a6df14
jjgouveia <gouvik.dev@gmail.com>
2022-08-24 18:51:45 -0300

    build: requisito 57

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,9 +3,16 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const email = 'teste';
   return (
     <div>
       <Header title="Profile" />
+      <div>
+        <h2 data-testid="profile-email">{email}</h2>
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button type="button" data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </div>
   );
