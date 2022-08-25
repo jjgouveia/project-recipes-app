09369f219067fd57305fd4020af72c2fed6cf99f
jjgouveia <gouvik.dev@gmail.com>
2022-08-25 14:21:14 -0300

    fix req quebrados

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -16,7 +16,6 @@ function AppProvider({ children }) {
         setLogin,
         apiResponse,
         setApiResponse,
-        // recipeAPI,
       } }
     >
       {children}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import AppContext from '../context/AppContext';
 
 export default function Profile() {
-  const emailObj = localStorage.getItem('user');
-  const user = JSON.parse(emailObj);
-
+  const { login } = useContext(AppContext);
   return (
     <div>
       <Header title="Profile" />
       <div>
-        <h2 data-testid="profile-email">{user.email}</h2>
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        <h2 data-testid="profile-email">{ login }</h2>
+        <a href="/done-recipes">
+          <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+        </a>
         <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button type="button" data-testid="profile-logout-btn">Logout</button>
       </div>
