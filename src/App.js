f4f8f47f53e3fbf523aa5e53e531693dfddbc8ad
Arthur-Liberatti <arthur_liberatti@hotmail.com>
2022-08-27 16:51:16 -0300

    requisto 20-23 test completo att

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
 import Drinks from './pages/Drinks';
@@ -9,30 +9,23 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from './pages/Profile';
 import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
     <div>
-
       <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/foods" component={ Foods } />
-        <Route
-          path="/drinks/:id-da-receita/in-progress"
-          render={ (props) => <Drinks { ...props } /> }
-        />
-        <Route
-          path="/foods/:id-da-receita/in-progress"
-          render={ (props) => <Foods { ...props } /> }
-        />
-        <Route exact path="/drinks/:id" component={ RecipeDetails } />
-        <Route exact path="/foods/:id" component={ RecipeDetails } />
+        <Route path="/drinks/:id/in-progress" component={ RecipeInProgress } />
+        <Route path="/foods/:id/in-progress" component={ RecipeInProgress } />
+        <Route path="/drinks/:id" component={ RecipeDetails } />
+        <Route path="/foods/:id" component={ RecipeDetails } />
         <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/foods" component={ Foods } />
         <Route exact path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/" component={ Login } />
       </Switch>
-
     </div>
   );
 }
