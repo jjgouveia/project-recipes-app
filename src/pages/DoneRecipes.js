ee9042435a9b43bf82b373d6e92dfb16b014df78
Leandro Camargo <leandro.bcamargo@gmail.com>
2022-08-28 16:26:41 -0300

    requisito 44 feito

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const getDoneRecipes = () => {
+    setDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+  };
+  useEffect(() => {
+    getDoneRecipes();
+  }, []);
   return (
     <div>
+
       <Header title="Done Recipes" />
+      {
+        doneRecipes.map((recipe, index) => (
+          <div key={ recipe.name }>
+            <div>
+              <button
+                type="button"
+                data-testid="filter-by-all-btn"
+              >
+                All
+              </button>
+              <button
+                type="button"
+                data-testid="filter-by-food-btn"
+              >
+                Food
+              </button>
+              <button
+                type="button"
+                data-testid="filter-by-drink-btn"
+              >
+                Drinks
+              </button>
+            </div>
+            <img
+              alt="foto-receita"
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              style={ { width: '200px' } }
+            />
+            <h3 data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</h3>
+            <h2 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h2>
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+            >
+              Compartilhar
+            </button>
+            {recipe.tags.map((tagName) => (
+              <p
+                key={ tagName }
+                data-testid={ `${index}-${tagName}-horizontal-tag` }
+              >
+                {tagName}
+              </p>))}
+          </div>
+        ))
+      }
     </div>
   );
 }
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function RecipeInProgress() {
+  const history = useHistory();
+  const handleClick = () => {
+    history.push('/done-recipes');
+  };
   return (
     <div>
       RecipeInProgress
-      <button type="button">
+      <button
+        data-testid="ingredient-step"
+        type="button"
+        onClick={ handleClick }
+      >
         Finish Recipe
       </button>
     </div>
